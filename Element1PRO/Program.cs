3c854cc52301ed92e466c8ea8bdc9adcf525c281
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-30 23:53:28 +0200

    Element1 complite, think about Pro

---
+++ Element1PRO/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+ сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+                    //Более продвинутая версия
+
+
+               
+Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Length];
+int count = 0;
+int[] b = new int [a];
+
+
+for ( int i = 0; i <= numbers.Length; i++)
+{
+    a = InputNumber("как положительное, так и отрицательное: ");
+    
+}
+
+Console.Write(b);
+
+int InputNumber(string name)
+{
+    Console.Write($"Введите число в массив {name},  ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
